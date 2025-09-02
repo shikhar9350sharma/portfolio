dc75239086a937d9666088fb93f707f22a8aa670
shikhar9350sharma <shikharsharma593@gmail.com>
2025-09-02 10:31:55 +0530

    name replacement

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -63,7 +63,7 @@ const Contact = () => {
               name="username" 
               value={formData.username} 
               onChange={handleChange}
-              placeholder='Jonny Jadoogar'
+              placeholder='John'
               className='px-2 py-1 rounded-lg bg-white/10 w-full focus:outline-none focus:ring-2 focus:ring-white'
             />
           </div>
@@ -108,7 +108,7 @@ const Contact = () => {
               name="username" 
               value={formData.username} 
               onChange={handleChange} 
-              placeholder="Jonny Jadoogar"
+              placeholder="John"
               className="px-3 py-2 rounded-lg bg-gray-100 text-black w-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-pink-400"
             />
           </div>
